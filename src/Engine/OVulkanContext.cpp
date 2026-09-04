a5b0710fe47bd047e4ad1bf8970d5c719a2462b0
Dhruv Dhangar <dhruvadhangar@gmail.com>
2026-09-04 18:38:09 +0530

    Added Logical and Physical Device, Family Indices

--- src/Engine/OVulkanContext.cpp
+++ src/Engine/OVulkanContext.cpp
@@ -1,4 +1,5 @@
 #include "OVulkanContext.h"
+#include "OLog.h"
 #include "VkBootstrap.h"
 #include <vulkan/vulkan_core.h>
 
@@ -35,7 +36,12 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL VulkanDebugCallback(VkDebugUtilsMessageSev
 bool OVulkanContext::Init(GLFWwindow* window) {
     m_Window = window;
 
-    return CreateInstance();
+    if (!CreateInstance()) return false;
+    if (!CreateSurface()) return false;
+    if (!ChoosePhysicalDevice()) return false;
+    if (!CreateDevice()) return false;
+
+    return true;
 }
 bool OVulkanContext::CreateInstance() {
     auto inst_ret = vkb::InstanceBuilder()
@@ -59,7 +65,85 @@ bool OVulkanContext::CreateInstance() {
     return true;
 }
 
+bool OVulkanContext::CreateSurface() {
+    VkResult result = glfwCreateWindowSurface(m_Instance, m_Window, nullptr, &m_Surface);
+
+    if (result != VK_SUCCESS) {
+        ORION_ERROR("Failed to create VUlkan Surfaces: {}", static_cast<int>(result));
+
+        return false;
+    }
+
+    ORION_INFO("Vulkan Surface Created");
+
+    return true;
+}
+
+bool OVulkanContext::ChoosePhysicalDevice() {
+    vkb::PhysicalDeviceSelector selector{ VKB_Instance };
+
+    auto phys_dev_ret = selector.set_minimum_version(1, 4).set_surface(m_Surface).select();
+
+    if (!phys_dev_ret) {
+        ORION_ERROR("Failed to select Physical Device: {}", phys_dev_ret.error().message());
+        return false;
+    }
+
+    VKB_PhysicalDevice = phys_dev_ret.value();
+
+    m_PhysicalDevice = VKB_PhysicalDevice.physical_device;
+
+    ORION_INFO("Physical Device Selected: {}", VKB_PhysicalDevice.properties.deviceName);
+
+    return true;
+}
+
+bool OVulkanContext::CreateDevice() {
+    vkb::DeviceBuilder device_builder{ VKB_PhysicalDevice };
+
+    auto device_ret = device_builder.build();
+    if (!device_ret) {
+        ORION_ERROR("Failed to create Vulkan Logical Device: {}", device_ret.error().message());
+        return false;
+    }
+
+    VKB_Device = device_ret.value();
+    m_Device = VKB_Device.device;
+
+    ORION_INFO("Created Logical Vulkan Device Successfully");
+
+    auto graphics_queue_ret = VKB_Device.get_queue(vkb::QueueType::graphics);
+
+    if (!graphics_queue_ret) {
+        ORION_ERROR("Failed to get Graphics Queue: ", graphics_queue_ret.error().message());
+        return false;
+    }
+
+    graphicsQueue = graphics_queue_ret.value();
+
+    auto graphics_queue_index_ret = VKB_Device.get_queue_index(vkb::QueueType::graphics);
+
+    if(!graphics_queue_index_ret){
+        ORION_ERROR("Failed to get graphics Queue Index: {}", graphics_queue_index_ret.error().message());
+        return false;
+  }
+
+    graphicsQueueIndex = graphics_queue_index_ret.value();
+
+    ORION_INFO("Choose Graphics Queue and Index Successfully");
+
+    return true;
+}
+
 void OVulkanContext::Shutdown() {
+
+    if (m_Device != VK_NULL_HANDLE) {
+        vkb::destroy_device(VKB_Device);
+    }
+
+    if (m_Surface != VK_NULL_HANDLE) {
+        vkDestroySurfaceKHR(m_Instance, m_Surface, nullptr);
+    }
     if (m_Instance != VK_NULL_HANDLE) {
         vkb::destroy_instance(VKB_Instance);
 
--- src/Engine/OVulkanContext.h
+++ src/Engine/OVulkanContext.h
@@ -1,8 +1,8 @@
+#define GLFW_INCLUDE_VULKAN
 #include "GLFW/glfw3.h"
 #include "VkBootstrap.h"
 #include <vulkan/vulkan_core.h>
 
-#include "OLog.h"
 
 namespace Orion {
 class OVulkanContext {
@@ -14,13 +14,32 @@ class OVulkanContext {
     bool Init(GLFWwindow *window);
     void Shutdown();
 
+    VkInstance GetInstance() const {return m_Instance;}
+    VkSurfaceKHR GetSurface() const {return m_Surface;}
+    
+    VkPhysicalDevice GetPhysicalDevice() const {return m_PhysicalDevice; }
+    VkDevice GetDevice() const {return m_Device;}
+
   private:
     bool CreateInstance();
+    bool CreateSurface();
+    bool ChoosePhysicalDevice();
+    bool CreateDevice();
 
   private:
     GLFWwindow *m_Window = nullptr;
 
     vkb::Instance VKB_Instance;
+    vkb::PhysicalDevice VKB_PhysicalDevice;
+    vkb::Device VKB_Device;
+
     VkInstance m_Instance = VK_NULL_HANDLE;
+    VkSurfaceKHR m_Surface = VK_NULL_HANDLE;
+    
+    VkPhysicalDevice m_PhysicalDevice = VK_NULL_HANDLE;
+    VkDevice m_Device = VK_NULL_HANDLE;
+    
+    VkQueue graphicsQueue = VK_NULL_HANDLE;
+    uint32_t graphicsQueueIndex = 0;
 };
 } // namespace Orion
